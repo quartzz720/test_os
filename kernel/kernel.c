4268c9d607717e44aa9d6f640e4cd7565aff691f
quartzz720 <quartzz720@gmail.com>
2026-03-25 17:09:11 +0200

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,18 +4,28 @@
 #include "kernel.h"
 
 // ============================================================================
-// Show welcome screen with loading animation (5 seconds)
+// Show welcome screen with loading animation
 // ============================================================================
 
 void show_welcome_screen(void) {
     clear();
     set_color(EFI_YELLOW, EFI_BGBLACK);
-    println(L"     _   ___                      ____  _____");
-    println(L"    / | / (_)___ _____ _____ _   / __ \\/ ___/");
-    println(L"   /  |/ / / __ `/ __ `/ __ `/  / / / /\\__ \\ ");
-    println(L"  / /|  / / /_/ / /_/ / /_/ /  / /_/ /___/ / ");
-    println(L" /_/ |_/_/\\__, /\\__, /\\__,_/   \\____//____/  ");
-    println(L"         /____//____/                        ");
+    println(L"        ,----,                                                      ");
+    println(L"      ,/   .`|                                ,----..               ");
+    println(L"    ,`   .'  :                      ___      /   /   \\   .--.--.    ");
+    println(L"  ;    ;     /                    ,--.'|_   /   .     : /  /    '.  ");
+    println(L".'___,/    ,'                     |  | :,' .   /   ;.  \\  :  /`. /  ");
+    println(L"|    :     |            .--.--.   :  : ' :.   ;   /  ` ;  |  |--`   ");
+    println(L";    |.';  ;   ,---.   /  /    '.;__,'  / ;   |  ; \\ ; |  :  ;_     ");
+    println(L"`----'  |  |  /     \\ |  :  /`./|  |   |  |   :  | ; | '\\  \\    `.  ");
+    println(L"    '   :  ; /    /  ||  :  ;_  :__,'| :  .   |  ' ' ' : `----.   \\ ");
+    println(L"    |   |  '.    ' / | \\  \\    `. '  : |__'   ;  \\; /  | __ \\  \\  | ");
+    println(L"    '   :  |'   ;   /|  `----.   \\|  | '.'|\\   \\  ',  / /  /`--'  / ");
+    println(L"    ;   |.' '   |  / | /  /`--'  /;  :    ; ;   :    / '--'.     /  ");
+    println(L"    '---'   |   :    |'--'.     / |  ,   /   \\   \\ .'    `--'---'   ");
+    println(L"             \\   \\  /   `--'---'   ---`-'     `---`                 ");
+    println(L"              `----'                                                ");
+    println(L"                                                                    ");
     set_color(EFI_WHITE, EFI_BGBLACK);
     println(L"");
     println(L"                 Welcome to TestOS!");
@@ -24,6 +34,7 @@ void show_welcome_screen(void) {
     println(L"");
     
     // Loading bar - approximately 5 seconds (50 iterations x ~100ms each)
+    // This shit is cringe and doesn't work lol, timer is based on core speed (?)
     print(L"  [");
     for (int i = 0; i < 50; i++) {
         print(L"=");
@@ -37,9 +48,7 @@ void show_welcome_screen(void) {
 }
 
 void kernel_main(void) {
-    // Show welcome screen
     show_welcome_screen();
-    
-    // Enter shell
     shell();
 }
+
